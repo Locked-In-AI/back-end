7f4b711f9dea5958184716465c90171f4c94b9c9
salah <salahdinjemal@gmail.com>
2024-03-02 00:37:09 -0600

    pep8

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
+
 class PersonalInfo(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
