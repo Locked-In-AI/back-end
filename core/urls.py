3d72a180fa22dd00237c259c70d4a78a644364ae
salah <salahdinjemal@gmail.com>
2024-03-08 21:43:07 -0600

    reorganize urls

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PersonalInfoViewSet, EducationViewSet, ExperienceViewSet, SkillViewSet, ProjectViewSet, CVViewSet, \
-    UserCVViewSet
+
+from .views import (CVViewSet,
+                    OptimizeCVViewSet)
 
 router = DefaultRouter()
 router.register(r'cv', CVViewSet)
 
-
 urlpatterns = [
     path('', include(router.urls)),
-    path('user-cv/<str:username>/', UserCVViewSet.as_view({'get': 'list'}), name='user-cv-list'),
+    path('optimize/', OptimizeCVViewSet.as_view(), name='optimize-cv'),
 ]
