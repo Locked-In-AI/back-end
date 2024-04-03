b1fd01a03a573cb5d9705731bf2d071b865c8d22
salah <salahdinjemal@gmail.com>
2024-04-02 20:30:53 -0500

    add application views

--- application/views.py
+++ application/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .models import JobApplication
+from .serializer import JobApplicationSerializer
 
-# Create your views here.
+
+class JobApplicationModelViewSet(viewsets.ModelViewSet):
+    queryset = JobApplication.objects.all()
+    serializer_class = JobApplicationSerializer
+    permission_classes = [IsAuthenticated]
