c5a0b75fc7be379cda126b9041d0dc9e37d2ba7c
salah <salahdinjemal@gmail.com>
2024-04-02 20:50:23 -0500

    fix form foreign key list to list only CV's current user created

--- application/admin.py
+++ application/admin.py
@@ -1,4 +1,13 @@
+from core.models import CV
 from django.contrib import admin
 from .models import JobApplication
 
-admin.site.register(JobApplication)
+
+class JobApplicationAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "cv":
+            kwargs["queryset"] = CV.objects.filter(user=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(JobApplication, JobApplicationAdmin)
